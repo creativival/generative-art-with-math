17b6f885d6dd053c9471d2ba6050d073a93adab0
creativival <creativival@email.com>
2021-12-14 06:12:01 +0900

    不要なファイルを削除

--- ellipse.py
+++
@@ -1,9 +0,0 @@
-if False:
-    from lib.Processing3 import *
-
-
-def setup():
-    size(800, 600)
-    ellipse(278, -100, 400, 400)
-    ellipse(120, 100, 110, 110)
-    ellipse(412, 60, 18, 18)
--- rect.py
+++
@@ -1,7 +0,0 @@
-if False:
-    from lib.Processing3 import *
-
-
-def setup():
-    size(800, 600)
-    rect(180, 60, 220, 40)
