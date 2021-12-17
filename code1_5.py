6681c52d7b0845f6bec3b24b28fe51701caaddc1
creativival <creativival@gmail.com>
2021-12-18 08:14:15 +0900

    code1_6.py マウスクリックで再描画

--- code1_5.py
+++ code1_5.py
@@ -1,6 +1,5 @@
 if False:
     from lib.Processing3 import *
-import util
 
 """
 長方形の分割によるユークリッド互除法の可視化（長方形を長方形に分割）
@@ -8,49 +7,70 @@ import util
 
 SIZE_X = 800
 SIZE_Y = 800
-NUM_A = 10
-NUM_B = 7
-RATIO = 1.0 * NUM_B / NUM_A
-wd = SIZE_X * RATIO
+num_a = 10
+num_b = 6
+ratio = 1.0 * num_b / num_a
+wd = SIZE_X * ratio
 position = PVector(0, 0)
 
 
 def setup():
-    global wd
     size(SIZE_X, SIZE_Y)
     colorMode(HSB, 1)
-    count = 0
+    div_rect()
 
+
+def div_rect():
+    global wd
+    count = 0
     while wd > 0.1:
         count += 1
         if count % 2 == 1:
             while position.x + wd < width + 0.1:
-                util.div_square(position, wd, RATIO)
+                div_square(position, wd)
                 position.x += wd
             wd = width - position.x
         else:
-            while position.y + wd < width * RATIO + 0.1:
-                util.div_square(position, wd, RATIO)
+            while position.y + wd < width * ratio + 0.1:
+                div_square(position, wd)
                 position.y += wd
-            wd = width * RATIO - position.y
+            wd = width * ratio - position.y
 
 
-# def div_square(_position, _wd):
-#     count = 0
-#     # ベクトルを作成（参照渡しにならないようにする）
-#     p = PVector(_position.x, _position.y)
-#     end_position = PVector(_wd + p.x, _wd + p.y)
-#     while _wd > 0.1:
-#         count += 1
-#         if count % 2 == 1:
-#             while p.x + _wd * RATIO < end_position.x + 0.1:
-#                 fill(color(random(1), 1, 1, 0.3))
-#                 rect(p.x, p.y, _wd * RATIO, _wd)
-#                 p.x += _wd * RATIO
-#             _wd = end_position.x - p.x
-#         else:
-#             while p.y + _wd / RATIO < end_position.y + 0.1:
-#                 fill(color(random(1), 1, 1, 0.3))
-#                 rect(p.x, p.y, _wd, _wd / RATIO)
-#                 p.y += _wd / RATIO
-#             _wd = end_position.y - p.y
+def div_square(_position, _wd):
+    count = 0
+    # ベクトルを作成（参照渡しにならないようにする）
+    p = PVector(_position.x, _position.y)
+    end_position = PVector(_wd + p.x, _wd + p.y)
+    while _wd > 0.1:
+        count += 1
+        if count % 2 == 1:
+            while p.x + _wd * ratio < end_position.x + 0.1:
+                fill(color(random(1), 1, 1, 0.3))
+                rect(p.x, p.y, _wd * ratio, _wd)
+                p.x += _wd * ratio
+            _wd = end_position.x - p.x
+        else:
+            while p.y + _wd / ratio < end_position.y + 0.1:
+                fill(color(random(1), 1, 1, 0.3))
+                rect(p.x, p.y, _wd, _wd / ratio)
+                p.y += _wd / ratio
+            _wd = end_position.y - p.y
+
+
+def mouseClicked():
+    global num_a, num_b, wd, ratio, position
+    num_a = int(random(1, 20))
+    num_b = int(random(1, 20))
+    while num_a <= num_b:
+        num_b = int(random(1, 20))
+    ratio = 1.0 * num_b / num_a
+    wd = SIZE_X * ratio
+    position = PVector(0, 0)
+    background(0, 0, 1)
+    div_rect()
+    print('numb_a = ' + str(num_a) + ', numb_b= ' + str(num_b))
+
+def draw():
+    pass
+
--- code1_6.py
+++ code1_6.py
@@ -8,10 +8,10 @@ import util
 
 SIZE_X = 500
 SIZE_Y = 500
-NUM_A = 10
-NUM_B = 6
-RATIO = 1.0 * NUM_B / NUM_A
-THRESHOLD = 40
+num_a = 10
+num_b = 6
+ratio = 1.0 * num_b / num_a
+threshold = 40
 
 
 def setup():
@@ -27,17 +27,17 @@ def div_square(_position, _wd):
     end_position = PVector(_wd + p.x, _wd + p.y)
     fill(color(random(1), 1, 1, 0.3))
     rect(p.x, p.y, _wd, _wd)
-    while _wd > THRESHOLD:
+    while _wd > threshold:
         count += 1
         if count % 2 == 1:
-            while p.x + _wd * RATIO < end_position.x + 0.1:
-                div_rect(p, _wd * RATIO)
-                p.x += _wd * RATIO
+            while p.x + _wd * ratio < end_position.x + 0.1:
+                div_rect(p, _wd * ratio)
+                p.x += _wd * ratio
             _wd = end_position.x - p.x
         else:
-            while p.y + _wd / RATIO < end_position.y + 0.1:
+            while p.y + _wd / ratio < end_position.y + 0.1:
                 div_rect(p, _wd)
-                p.y += _wd / RATIO
+                p.y += _wd / ratio
             _wd = end_position.y - p.y
 
 
@@ -45,10 +45,10 @@ def div_rect(_position, _wd):
     count = 0
     # ベクトルを作成（参照渡しにならないようにする）
     p = PVector(_position.x, _position.y)
-    end_position = PVector(_wd + p.x, _wd / RATIO + p.y)
+    end_position = PVector(_wd + p.x, _wd / ratio + p.y)
     fill(color(random(1), 1, 1, 0.3))
-    rect(p.x, p.y, _wd, _wd / RATIO)
-    while _wd > THRESHOLD:
+    rect(p.x, p.y, _wd, _wd / ratio)
+    while _wd > threshold:
         count += 1
         if count % 2 == 0:
             while p.x + _wd < end_position.x + 0.1:
@@ -60,3 +60,19 @@ def div_rect(_position, _wd):
                 div_square(p, _wd)
                 p.y += _wd
             _wd = end_position.y - p.y
+
+
+def mouseClicked():
+    global num_a, num_b, threshold, ratio
+    num_a = int(random(1, 20))
+    num_b = int(random(1, 20))
+    while num_a == num_b:
+        num_b = int(random(1, 20))
+    threshold = int(random(10, 200))
+    print('num_a = ' + str(num_a) + ', num_b= ' + str(num_b) + ', threshold = ' + str(threshold))
+    ratio = 1.0 * num_a / num_b
+    background(0, 0, 1)
+    div_square(PVector(0, 0), width)
+
+def draw():
+    pass
